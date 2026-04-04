179cc02519ee0558c7ce66e1eddf85c9dd137ba1
AxelMonge1 <axel.monge262799@potros.itson.edu.mx>
2026-04-04 14:30:21 -0600

    Main

---
+++ Proyecto_BDAv/src/main/java/org/itson/proyecto_bdav/Proyecto_BDAv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.proyecto_bdav;
+
+/**
+ *
+ * @author axelm
+ */
+public class Proyecto_BDAv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
